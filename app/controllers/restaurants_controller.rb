518aa76b2cd5ccd25399ddb2fccbad7cb4725b27
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-11 15:46:22 +0100

    configuration on controller restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,11 @@
 class RestaurantsController < ApplicationController
+
   def index
+    @Restaurants = Restaurant.all
   end
 
   def show
+    @Restaurant = Restaurant.find(params[:id])
   end
 
   def new
