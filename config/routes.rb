978b66ee1d1a79662dd77a10012a464090a317c8
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-11 15:40:18 +0100

    add validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
   resources :restaurants do
     resources :reviews, only: [:new, :create]
   end
