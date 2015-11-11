da467c94186d63cb7eb2e9df7e219d2936e32ef7
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-11 15:57:23 +0100

    worked on restaurant & review controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,20 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
